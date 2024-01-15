98d6062ca066eec85ba1c9c1f1e818e7b2dd5c49
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-01-15 15:32:43 +0000

    refactor: refactoring the code

--- Training/frizzbuzz.js
+++ Training/frizzbuzz.js
@@ -1,20 +1,13 @@
 function frizzbuzz(num){
-    let counter =1;
     let n=1;
+    let printMessage;
     while(n<=num){
-        if(n%15===0){
-            console.log("FrizzBuzz")
-        }
-        else if(n%3===0){
-            console.log("Frizz")
-        }
-        else if(n%5===0){
-            console.log("Buzz")
-        }
-        else{
-            console.log(n)
-        }
+        if(n%15===0) printMessage="FrizzBuzz";
+        else if(n%3===0) printMessage = "Frizz";
+        else if(n%5===0) printMessage="Buzz";
+        else printMessage = n;
         n+=1;
+        console.log(printMessage)
     }
 
 }
