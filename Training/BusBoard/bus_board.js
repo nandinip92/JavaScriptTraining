403bec82bd849e71d482fbc825b9175178ee2eeb
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-01-22 11:30:35 +0000

    feat: Adding code to get the StopPoints from the given Post code

--- Training/BusBoard/bus_board.js
+++ Training/BusBoard/bus_board.js
@@ -1,39 +1,101 @@
 import fetch from "node-fetch";
-import promptSync from 'prompt-sync';
+import promptSync from "prompt-sync";
+const prompt = promptSync();
 
-
-async function tflAPIFetch(stopCode){
-    const response = await fetch(`https://api.tfl.gov.uk/StopPoint/${stopCode}/Arrivals`);
-    const arrivalPredictionData = await response.json();
-    return arrivalPredictionData;
+async function tflAPIFetch(url) {
+  const response = await fetch(url);
+  const apiData = await response.json();
+  return apiData;
 }
 
-async function bustStop(){
-    //   const stopCode = '490008660N';
-    const prompt = promptSync();
-    const stopCode = prompt("Please enter the stopCode for the bus station needed: ");
-    const apiData = await tflAPIFetch(stopCode);
+async function bustStop(stopCode) {
+  // const stopCode = '490008660N';
+  // const stopCode = prompt("Please enter the stopCode for the bus station needed: ");
+  console.log(`StopCode:${stopCode}`);
+  const tflURL = `https://api.tfl.gov.uk/StopPoint/${stopCode}/Arrivals`;
+  const apiData = await tflAPIFetch(tflURL);
 
-    //const requiredFields = ["lineId","destinationName","timestamp","expectedArrival"];
-    const requiredFields = ["lineId","destinationName","expectedArrival"];
-    const filterFields=(busData)=>Object.keys(busData).filter(key => requiredFields.includes(key)).reduce((obj,key)=>{obj[key]=busData[key]; return obj},{});
-    const requiredData = apiData.map(entry=>filterFields(entry));
+  //const requiredFields = ["lineId","destinationName","timestamp","expectedArrival"];
+  const requiredFields = [
+    "lineId",
+    "destinationName",
+    "expectedArrival",
+    "stationName",
+    "direction",
+  ];
+  const filterFields = (busData) =>
+    Object.keys(busData)
+      .filter((key) => requiredFields.includes(key))
+      .reduce((obj, key) => {
+        obj[key] = busData[key];
+        return obj;
+      }, {});
+  const requiredData = apiData.map((entry) => filterFields(entry));
+  //console.log(requiredData);
 
-    const dataWithArrivalMins = requiredData.map(entry=>{
-        const diff_milliseconds = new Date(entry.expectedArrival)-new Date();
-        entry["arrivalInMinutes"]= Math.floor((diff_milliseconds / 1000 / 60) % 60);
-        return entry;
-    })
-    const nextBusList = dataWithArrivalMins.sort((x,y)=>x["arrivalInMinutes"]-y["arrivalInMinutes"]);
-    console.log("Next 5 Busses:")
-    for(let i =0;i<=4;i++){
-        console.log(`${i+1} : ${JSON.stringify(nextBusList[i])}`);
-    }
+  const dataWithArrivalMins = requiredData.map((entry) => {
+    const diff_milliseconds = new Date(entry.expectedArrival) - new Date();
+    entry["arrivalInMinutes"] = Math.floor(
+      (diff_milliseconds / 1000 / 60) % 60
+    );
+    return entry;
+  });
+  // console.log(dataWithArrivalMins);
+  const nextBusList = dataWithArrivalMins.sort(
+    (x, y) => x["arrivalInMinutes"] - y["arrivalInMinutes"]
+  );
+  //console.log(nextBusList);
+  console.log("Next 5 Busses:");
+  for (let i = 0; i <= 4; i++) {
+    console.log(`${i + 1} : ${JSON.stringify(nextBusList[i])}`);
+  }
 }
 
-bustStop();
+//bustStop();
 
+async function getPostCode() {
+  //const postCode = "NW51TL";
+  const postCode = prompt("Enter the postcode: ");
+  const postCodeUrl = `https://api.postcodes.io/postcodes/${postCode}`;
+  const postCodeData = await tflAPIFetch(postCodeUrl);
+  return postCodeData;
+}
+async function getLatLong() {
+  const postCodeData = await getPostCode();
+  //console.log(postCodeData);
+  const latidude = postCodeData.result.latitude;
+  const longitude = postCodeData.result.longitude;
+  return { lat: latidude, lon: longitude };
+}
+async function getStopCodeId() {
+  const coOrds = await getLatLong();
+  const stopCodeUrl = `https://api.tfl.gov.uk/StopPoint/?lat=${coOrds.lat}&lon=${coOrds.lon}&stopTypes=NaptanOnstreetBusCoachStopPair`;
+  const stopCodeData = await tflAPIFetch(stopCodeUrl);
+  //console.log(stopCodeData);
+  const stopPoints = stopCodeData.stopPoints;
 
+  const requiredFields = ["commonName", "lineGroup"];
+  const filterFields = (sp) =>
+    Object.keys(sp)
+      .filter((key) => requiredFields.includes(key))
+      .reduce((obj, key) => {
+        obj[key] = sp[key];
+        return obj;
+      }, {});
+  const nearestStopsData = stopPoints.map((sp) => filterFields(sp));
+  nearestStopsData.forEach(async (ns) => {
+    console.log(ns.commonName);
+    const lineGroup = ns.lineGroup.map((lg) => lg.naptanIdReference);
+    //  console.log(lineGroup);
+    const result = await Promise.all(
+      lineGroup.map(async (stopId) => await bustStop(stopId))
+    );
+  });
+  //console is printing both the bus
+  //console.log(stopIdsArr)
+}
+
+getStopCodeId();
 
 // const expectedArraival = '2024-01-18T15:50:10Z'
 // console.log("Current Timestamp:",new Date()) // current date and time in ISO format
